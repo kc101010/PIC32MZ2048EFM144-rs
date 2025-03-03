56a8eb063188387c8acc6dab25c4cd83a7cc554a
kc101010 <blueboi.kc@gmail.com>
2025-03-03 11:37:45 +0000

    upload new bare-metal app

--- pic32mz-blinky/src/main.rs
+++ pic32mz-blinky/src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
+#![feature(asm)]
 
+use mips_mcu::interrupt;
 use mips_rt::entry;
 use panic_halt as _;
 use pic32mz2048efm144_pac::Peripherals;
@@ -69,14 +71,36 @@ pub static SEQSFRS: [u16; 2] = [SEQ_NO, !SEQ_NO];
 fn main() -> ! {
     let p = unsafe { Peripherals::steal() };
 
-    //Set ANSEL B reg to 0, set PORT B pins to digital
-    p.portb.anselbset().write(|w| unsafe{w.bits(0)});
+    unsafe{
+        interrupt::enable_mv_irq();
+        p.int.intconset().write(|w| {w.mvec().bit(true)});
+        p.int.priss().write(|w| {w.ss0().bit(false)});
+        interrupt::enable();
+    }
 
-    //Clear bit 0 of TRISB, make PORT B bit 0 an output
-    p.portb.trisbclr().write(|w| unsafe{w.bits(1 << 0)});
+    //init timer 
+    p.tmr1.pr1().write(|w| unsafe {w.bits(0xffff)});
+    p.tmr1.t1con().write(|w| unsafe {w.on().bit(true).tckps().bits(0b11)});
 
-    //Set bit 0 of PORT B to 1
-    p.portb.latbset().write(|w| unsafe{w.bits(1 << 0)});
+    //init oscillator?
+    p.int.ifs0clr().write(|w| {w.t1if().bit(true)});
+    p.int.iec0set().write(|w| {w.t1ie().bit(true)});
+    p.int.ipc1().modify(|_, w| unsafe{w.t1ip().bits(1)});
 
-    loop {}
+    //set RB3 to digital output
+    p.portb.anselb().write(|w| unsafe{ w.bits(0) });
+    p.portb.trisb().write(|w| unsafe{ w.bits(0) });
+
+    //toggle LED with delay
+    loop {
+        p.portb.latb().write(|w| unsafe{w.bits(0)});
+
+        let mut i: i32= 0;
+        while i < 200000 {
+            i += 1;
+            //unsafe{ asm!("nop") };
+        }
+    
+        p.portb.latb().write(|w| unsafe{w.bits(1)});
+    }
 }
