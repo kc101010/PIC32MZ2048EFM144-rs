3de6663f1d1b130e9ae4843e15408b96613aabcb
kc101010 <blueboi.kc@gmail.com>
2026-05-12 21:33:52 +0100

    rework app into MVP for getting an LED running

--- pic32mz-blinky/src/main.rs
+++ pic32mz-blinky/src/main.rs
@@ -70,37 +70,12 @@ pub static SEQSFRS: [u16; 2] = [SEQ_NO, !SEQ_NO];
 #[entry]
 fn main() -> ! {
     let p = unsafe { Peripherals::steal() };
-
-    unsafe{
-        interrupt::enable_mv_irq();
-        p.int.intconset().write(|w| {w.mvec().bit(true)});
-        p.int.priss().write(|w| {w.ss0().bit(false)});
-        interrupt::enable();
-    }
-
-    //init timer 
-    p.tmr1.pr1().write(|w| unsafe {w.bits(0xffff)});
-    p.tmr1.t1con().write(|w| unsafe {w.on().bit(true).tckps().bits(0b11)});
-
-    //init oscillator?
-    p.int.ifs0clr().write(|w| {w.t1if().bit(true)});
-    p.int.iec0set().write(|w| {w.t1ie().bit(true)});
-    p.int.ipc1().modify(|_, w| unsafe{w.t1ip().bits(1)});
-
-    //set RB3 to digital output
-    p.portb.anselb().write(|w| unsafe{ w.bits(0) });
-    p.portb.trisb().write(|w| unsafe{ w.bits(0) });
+    
+    //Set PORTH GPIO as output
+    p.porth.trish().write(|w| unsafe{w.bits(0)});
+    p.porth.lath().write(|w| unsafe{w.bits(1)});
 
     //toggle LED with delay
     loop {
-        p.portb.latb().write(|w| unsafe{w.bits(0)});
-
-        let mut i: i32= 0;
-        while i < 200000 {
-            i += 1;
-            //unsafe{ asm!("nop") };
-        }
-    
-        p.portb.latb().write(|w| unsafe{w.bits(1)});
     }
 }
